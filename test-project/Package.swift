6f9641843579adf54c5722575d7b5273c606d0b6
Jesse L. Zamora <xtremekforever@gmail.com>
2024-12-21 06:52:45 -0500

    Add remaining graduated packages. Does not build for armv7 due to missing symbols, but works for aarch64

--- test-project/Package.swift
+++ test-project/Package.swift
@@ -7,6 +7,8 @@ let package = Package(
     name: "test-project",
     dependencies: [
         // https://www.swift.org/sswg/incubated-packages.html
+
+        // graduated packages
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.77.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.29.0"),
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.35.0"),
@@ -15,6 +17,15 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-ssh.git", from: "0.9.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.2"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.5.0"),
+        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.23.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.24.0"),
+        .package(url: "https://github.com/swift-server-community/APNSwift.git", from: "6.0.1"),
+        .package(url: "https://github.com/apple/swift-statsd-client.git", from: "1.0.1"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.24.2"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.10.0"),
+        .package(url: "https://github.com/soto-project/soto.git", from: "7.3.0"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.1.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.0"),
     ],
     targets: [
         .executableTarget(name: "hello-world"),
@@ -29,6 +40,15 @@ let package = Package(
                 .product(name: "NIOSSH", package: "swift-nio-ssh"),
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "Metrics", package: "swift-metrics"),
+                .product(name: "PostgresNIO", package: "postgres-nio"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                .product(name: "APNS", package: "apnswift"),
+                .product(name: "StatsdClient", package: "swift-statsd-client"),
+                .product(name: "GRPC", package: "grpc-swift"),
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "SotoACM", package: "soto"),
+                .product(name: "JWTKit", package: "jwt-kit"),
+                .product(name: "Vapor", package: "vapor"),
             ]
         ),
     ]
--- test-project/Sources/sswg-incubated-graduated/main.swift
+++ test-project/Sources/sswg-incubated-graduated/main.swift
@@ -1,3 +1,8 @@
+import APNS
+import AsyncHTTPClient
+import Crypto
+import GRPC
+import JWTKit
 import Logging
 import Metrics
 import NIO
@@ -13,6 +18,10 @@ import NIOPosix
 import NIOSSL
 import NIOTLS
 import NIOTransportServices
+import PostgresNIO
+import SotoACM
+import StatsdClient
+import Vapor
 
-let logger = Logger(label: "sswg-incubated-packages")
-logger.info("Hello SSWG!")
+let logger = Logger(label: "sswg-incubated-graduated")
+logger.info("Hello SSWG Incubated: Graduated Packages!")
