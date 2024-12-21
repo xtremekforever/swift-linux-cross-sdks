74198fd8253a923e91c94768e1d242224589bbab
Jesse L. Zamora <xtremekforever@gmail.com>
2024-12-21 13:19:55 -0500

    Fix a few typos in new code

--- test-sdk.sh
+++ test-sdk.sh
@@ -3,18 +3,21 @@
 TARGET_ARCH=${TARGET_ARCH:=x86_64}
 
 SWIFT_VERSION=$1
+SWIFT_VERSION=$(echo $SWIFT_VERSION | xargs)
 if [ -z $SWIFT_VERSION ]; then
     echo "Swift version is required! (e.g.: 6.0.3)"
     exit -1
 fi
 
 DISTRIBUTION_NAME=$2
+DISTRIBUTION_NAME=$(echo $DISTRIBUTION_NAME | xargs)
 if [ -z $DISTRIBUTION_NAME ]; then 
     echo "Distribution name is required! (e.g.: ubuntu, rhel)"
     exit -1
 fi
 
 DISTRIBUTION_VERSION=$3
+DISTRIBUTION_VERSION=$(echo $DISTRIBUTION_VERSION | xargs)
 if [ -z $DISTRIBUTION_VERSION ]; then
     echo "Distribution version is required! (e.g.: jammy, bookworm)"
     exit -1
