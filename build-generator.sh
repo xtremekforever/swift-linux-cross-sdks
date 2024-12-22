edd889656a1b1cbf47313f6f9c0b0341df511888
Jesse L. Zamora <xtremekforever@gmail.com>
2024-12-22 13:54:19 -0500

    Fix updating SDK generator

--- build-generator.sh
+++ build-generator.sh
@@ -6,13 +6,14 @@ SDK_GENERATOR_DIR=swift-sdk-generator
 
 # This should default to develop, which has the latest advances in the generator
 if [ -d $SDK_GENERATOR_DIR ]; then
+    cd $SDK_GENERATOR_DIR
     git pull origin
 else
     git clone https://github.com/xtremekforever/swift-sdk-generator.git
+    cd $SDK_GENERATOR_DIR
 fi
 
 # Build
-cd $SDK_GENERATOR_DIR
 swift build -c release
 
 # Deploy
