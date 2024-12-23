71aa674c87dbfb6eeb7bbff5ee173b30404b92d0
Jesse L. Zamora <xtremekforever@gmail.com>
2024-12-22 21:52:29 -0500

    Only include usr/lib/swift/linux in deb file

--- build-deb.sh
+++ build-deb.sh
@@ -6,11 +6,10 @@ echo "Creating deb artifact of the Swift ${SWIFT_VERSION} runtime for ${LINUX_PL
 PACKAGE_NAME=swift-runtime_${SWIFT_VERSION}-${DISTRIBUTION_NAME}-${DISTRIBUTION_VERSION}_${LINUX_PLATFORM}
 ARTIFACT_PATH=artifacts/${PACKAGE_NAME}
 mkdir -p ${ARTIFACT_PATH}/DEBIAN
-mkdir -p ${ARTIFACT_PATH}/usr/lib
+mkdir -p ${ARTIFACT_PATH}/usr/lib/swift
 
 # Copy files
-cp -r ${SDK_SYSROOT_PATH}/usr/lib/swift ${ARTIFACT_PATH}/usr/lib
-rm -rf ${ARTIFACT_PATH}/usr/lib/swift/clang
+cp -r ${SDK_SYSROOT_PATH}/usr/lib/swift/linux ${ARTIFACT_PATH}/usr/lib/swift
 
 # Create control file
 cat <<EOT > ${ARTIFACT_PATH}/DEBIAN/control
