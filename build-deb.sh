d31c399c5afb226a9b4f194260e71de4e951c59e
Jesse L. Zamora <xtremekforever@gmail.com>
2025-03-30 13:59:25 -0400

    Install binutils when building deb file

--- build-deb.sh
+++ build-deb.sh
@@ -13,7 +13,11 @@ mkdir -p ${ARTIFACT_PATH}/usr/lib/swift/linux
 cp -rL ${SDK_SYSROOT_PATH}/usr/lib/swift/linux/*.so ${ARTIFACT_PATH}/usr/lib/swift/linux
 
 # Strip files
-$STRIP_BINARY -s ${ARTIFACT_PATH}/usr/lib/swift/linux/*.so
+sudo apt install -y binutils-${BINUTILS_NAME/_/-}
+if [ $(which $STRIP_BINARY) ]; then
+    echo "Stripping libraries for deb package..."
+    $STRIP_BINARY -s ${ARTIFACT_PATH}/usr/lib/swift/linux/*.so
+fi
 
 # Create control file
 cat <<EOT > ${ARTIFACT_PATH}/DEBIAN/control
